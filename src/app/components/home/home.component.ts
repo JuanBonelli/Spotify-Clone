adc4770f3b13d8db1b23f5334e539c32b0b5ff6c
JuanBonelli <juan.bonelli003@gmail.com>
2024-12-01 17:51:00 -0300

    feat(playlist-page): add playlist page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { SongInformationComponent } from '../song-information/song-information.component';
 import { LibraryComponent } from '../library/library.component';
-import { RecommendationsComponent } from '../recommendations/recommendations.component';
+import { PlaylistComponent } from "../playlist/playlist.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SongInformationComponent , LibraryComponent, RecommendationsComponent ],
+  imports: [SongInformationComponent, LibraryComponent, PlaylistComponent, PlaylistComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/components/playlist/playlist.component.ts
+++ src/app/components/playlist/playlist.component.ts
@@ -1,12 +1,23 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroArrowDownCircle, heroArrowPath, heroListBullet } from '@ng-icons/heroicons/outline';
+import { heroPauseCircleSolid, heroPlayCircleSolid } from '@ng-icons/heroicons/solid';
+import { heroHashtagMini, heroClockMini } from '@ng-icons/heroicons/mini';
+
 
 @Component({
   selector: 'app-playlist',
   standalone: true,
-  imports: [],
+  imports: [NgIcon, NgIf],
+  viewProviders: [provideIcons({ heroPlayCircleSolid, heroPauseCircleSolid , heroArrowPath, heroListBullet, heroArrowDownCircle, heroClockMini, heroHashtagMini})],
   templateUrl: './playlist.component.html',
   styleUrl: './playlist.component.scss'
 })
 export class PlaylistComponent {
+  isPlaying: boolean = false;
 
+  onPlayPause() {
+    this.isPlaying = !this.isPlaying;
+  }
 }
